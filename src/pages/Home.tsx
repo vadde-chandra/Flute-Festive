e758312c4ca761db9f22e45130678ee2ccf7cac1
vadde-chandra <chandra4832@gmail.com>
2025-12-20 10:24:03 +0530

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -105,7 +105,7 @@ export default function Home({ onRegisterClick, onSignupClick }: HomeProps) {
         <img
           src="/final_classical.png"
           alt="Sesha Latha Manda"
-          className="w-150 h-48 object-cover rounded-lg mb-6"
+          className="w-330 h-48 object-cover rounded-lg mb-6"
         />
 
         <h3 className="text-2xl font-bold text-gray-900 mb-4">
